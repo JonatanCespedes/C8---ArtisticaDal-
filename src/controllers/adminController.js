0e32878375f68a3e0e1d71015f20a901d767b240
Jonatan Cespedes <retratos.jonatan@gmail.com>
2021-09-01 19:44:32 -0300

    Express validator

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,4 +1,5 @@
 const { products, categories, writeProductsJSON } = require('../data/dataBase');
+const { validationResult } = require('express-validator')
 
 let subcategories = [];
 products.forEach(product => {
@@ -26,39 +27,58 @@ module.exports = {
         })
     }, 
     productStore: (req, res) => {
-        let lastId = 1;
+        let errors = validationResult(req)
 
-        products.forEach(product => {
-            if(product.id > lastId){
-                lastId = product.id
+        if(errors.isEmpty()){
+            let lastId = 1;
+
+            products.forEach(product => {
+                if(product.id > lastId){
+                    lastId = product.id
+                }
+            })
+    
+            let arrayImages = [];
+            if(req.files){
+                req.files.forEach(image => {
+                    arrayImages.push(image.filename)
+                })
             }
-        })
-
-        let {
-            name, 
-            price, 
-            discount, 
-            category, 
-            subcategory, 
-            description
-            } = req.body;
-
-        let newProduct = {
-            id: lastId + 1,
-            name,
-            price,
-            description,
-            discount,
-            category,
-            subcategory,
-            image: req.file ? [req.file.filename] : "default-image.png"
-        };
-
-        products.push(newProduct);
-
-        writeProductsJSON(products)
-
-        res.redirect('/admin/products')
+    
+            let {
+                name, 
+                price, 
+                discount, 
+                category, 
+                subcategory, 
+                description
+                } = req.body;
+    
+            let newProduct = {
+                id: lastId + 1,
+                name,
+                price,
+                description,
+                discount,
+                category,
+                subcategory,
+                image: arrayImages.length > 0 ? arrayImages : "default-image.png"
+            };
+    
+            products.push(newProduct);
+    
+            writeProductsJSON(products)
+    
+            res.redirect('/admin/products')
+        }else {
+            res.render("adminProductCreateForm", {
+                subcategories,
+                categories,
+                errors: errors.mapped(),
+                old: req.body
+            })
+        }
+     
     }, 
     productEdit: (req, res) => {
         let product = products.find(product => product.id === +req.params.id)
@@ -69,6 +89,13 @@ module.exports = {
         })
     },
     productUpdate: (req, res) => {
+
+        let arrayImages = [];
+        if(req.files){
+            req.files.forEach(image => {
+                arrayImages.push(image.filename)
+            })
+        }
         
         let {
             name, 
@@ -88,9 +115,12 @@ module.exports = {
                 product.discount = discount,
                 product.category = category,
                 product.subcategory = subcategory,
-                product.image = req.file ? [req.file.filename] : product.image
+                product.image = arrayImages > 0 ? arrayImages : product.image
+                console.log(product)
             }
         })
+
+
 
         writeProductsJSON(products)
 
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -10,6 +10,7 @@ let {
     productDestroy,
     productUpdate} = require('../controllers/adminController');
 let uploadProductFile = require('../middlewares/uploadProductsFiles')
+let productValidator = require('../validations/productCreateValidator')
 
 
 /* GET - Admin Signin */
@@ -20,10 +21,10 @@ router.get('/index', dashboard);
 router.get('/products', products);
 /* Create Product*/
 router.get('/products/create', productsCreate);
-router.post('/products/create', uploadProductFile.single("image"),productStore);
+router.post('/products/create', uploadProductFile.array("images"), productValidator,productStore);
 /* Edit Product*/
 router.get('/products/edit/:id', productEdit);
-router.put('/products/edit/:id', uploadProductFile.single("image"), productUpdate);
+router.put('/products/edit/:id', uploadProductFile.array("images"), productValidator,productUpdate);
 /* Edit Product*/
 router.delete('/products/delete/:id', productDestroy);
 
