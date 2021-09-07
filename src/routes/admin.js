8e31dd57b394b29d2c25127f78eac3d65ba963c8
Jonatan Cespedes <retratos.jonatan@gmail.com>
2021-09-06 21:02:10 -0300

    Middlewares rutas

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -11,19 +11,20 @@ let {
     productUpdate} = require('../controllers/adminController');
 let uploadProductFile = require('../middlewares/uploadProductsFiles')
 let productValidator = require('../validations/productCreateValidator')
+let userAdminCheck = require('../middlewares/userAdminCheck')
 
 
 /* GET - Admin Signin */
 router.get('/', signin);
 /* GET - Admin Dashboard */
-router.get('/index', dashboard);
+router.get('/index', userAdminCheck,dashboard);
 /* GET - Admin products*/
-router.get('/products', products);
+router.get('/products', userAdminCheck,products);
 /* Create Product*/
-router.get('/products/create', productsCreate);
+router.get('/products/create', userAdminCheck,productsCreate);
 router.post('/products/create', uploadProductFile.array("images"), productValidator,productStore);
 /* Edit Product*/
-router.get('/products/edit/:id', productEdit);
+router.get('/products/edit/:id', userAdminCheck,productEdit);
 router.put('/products/edit/:id', uploadProductFile.array("images"), productValidator,productUpdate);
 /* Edit Product*/
 router.delete('/products/delete/:id', productDestroy);
--- src/routes/users.js
+++ src/routes/users.js
@@ -11,20 +11,22 @@ const { register,
 const loginValidator = require('../validations/loginValidator')
 const registerValidator = require('../validations/registerValidator')
 const uploadUserAvatar = require('../middlewares/uploadUserAvatar')
+const userSessionCheck = require('../middlewares/userSessionCheck')
+const userLog = require('../middlewares/userLog')
 
 /* GET - Register form */
-router.get('/register', register);
+router.get('/register', userLog,register);
 router.post('/register', uploadUserAvatar.single('avatar'), registerValidator, processRegister);
 
 /* GET - Login form */
-router.get('/login', login);
+router.get('/login', userLog, login);
 router.post('/login', loginValidator, processLogin);
-router.get('/logout', logout);
+router.get('/logout', userSessionCheck, logout);
 
 /* GET - User profile */
-router.get('/profile', profile)
+router.get('/profile', userSessionCheck,profile)
 
-router.get('/profile/edit/:id', profileEdit)
+router.get('/profile/edit/:id', userSessionCheck,profileEdit)
 router.put('/profile/edit/:id', uploadUserAvatar.single('avatar'),updateProfile)
 
 module.exports = router
