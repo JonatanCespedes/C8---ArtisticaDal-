e4040a0b978b92a57210813085828e450d73e9fa
Jona <develop.jec@gmail.com>
2021-09-23 19:41:51 -0300

    cookies session

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ let path = require('path');
 let cookieParser = require('cookie-parser')
 let methodOverride = require('method-override');
 let session = require('express-session');
+let localsCheck = require('./middlewares/localsCheck');
+
 
 /* ENRUTADORES */
 let indexRouter = require('./routes/index');
@@ -11,7 +13,7 @@ let productsRouter = require('./routes/products');
 let adminRouter = require('./routes/admin');
 let usersRouter = require('./routes/users');
 
-/* Middleware */
+/* Middlewares */
 app.use(express.static('./public'));
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
@@ -20,9 +22,10 @@ app.use(cookieParser())
 app.use(session({
     secret: "artisticaDali",
     resave: false,
-    saveUninitialized: true,
-    cookie: { maxAge: 30000 }
+    saveUninitialized: true
 }))
+app.use(localsCheck)
+
 
 /* VIEWS */
 app.set('view engine', 'ejs');
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 let controller = require('../controllers/indexController');
+let cookieCheck = require('../middlewares/cookieCheck');
 
 /* GET - Home */
-router.get('/', controller.index)
-
+router.get('/', cookieCheck,controller.index)
 
 module.exports = router;
